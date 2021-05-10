6c2f8457e48a16ea21843d325872258340ea968a
sietze1983 <sietze1983@hotmail.com>
2021-05-10 20:04:17 +0200

    huiswerk tech-it

--- main.js
+++ main.js
@@ -161,3 +161,103 @@ const inventory = [
     sold: 8,
   },
 ];
+
+// Opdracht 1a
+const tvToSell = inventory.map ((tvToBeSold) => {
+  const tvStock = tvToBeSold.originalStock;
+  const tvSold = tvToBeSold.sold;
+  return tvStock - tvSold;
+})
+let total = 0
+tvToSell.map(function(currentValue) {
+  total += currentValue
+})
+console.log('map: ' + total);
+
+// Opdracht 1b
+let header1 = document.getElementById("toBeSold")
+header1.innerText = total;
+
+// Opdracht 2a
+const tvTypes = inventory.map((tvtype) => {
+  return tvtype.type
+})
+console.log(tvTypes)
+
+// Opdracht 2b
+const tvSoldOut = inventory.filter((outOfStock) => {
+  return outOfStock.originalStock === outOfStock.sold;
+})
+console.log(tvSoldOut)
+
+// Opdracht 2c
+const ambilight = inventory.find((isItAmbilight) => {
+  return isItAmbilight.options.ambiLight === true;
+
+})
+console.log(ambilight)
+
+// Opdracht 2d
+const price = inventory.sort ((a, b) => {
+  if (a.price > b.price) {
+    return 1
+  }
+  if (a.price < b.price) {
+    return -1
+  }
+})
+console.log(price)
+
+// Opdracht 3a
+const totalAmountOfRevenue = inventory.map((totalRevenue) => {
+  const originalStock = totalRevenue.originalStock;
+  const sold = totalRevenue.sold;
+  const price = totalRevenue.price;
+  return (originalStock + sold) * price
+})
+let totalRevenue = 0
+totalAmountOfRevenue.map((currentValue) => {
+  totalRevenue += currentValue
+})
+let header2 = document.getElementById("revenue")
+header2.innerText = totalRevenue
+console.log('map: ' + totalRevenue)
+
+// Opdracht 3b
+const totalEarned = inventory.map((totalEarnings) => {
+  const sold = totalEarnings.sold;
+  const price = totalEarnings.price;
+  return sold * price;
+})
+let totalEarnedSold = 0;
+totalEarned.map((currentValue) => {
+  totalEarnedSold += currentValue
+})
+let header3 = document.getElementById("earned")
+header3.innerText = totalEarnedSold;
+console.log(totalEarnedSold)
+
+// Opdracht 4
+let philipsType = "";
+
+for (let i=0; i < inventory.length; i++){
+  if (inventory[i].type === "43PUS6504/12") {
+    philipsType = inventory[i].type;
+  }
+}
+console.log(philipsType)
+ let div1 = document.getElementById("type1");
+div1.innerText = philipsType
+
+let Nikkei = "";
+
+for (let i=0; i < inventory.length; i++){
+  if (inventory[i].type === "NH3216SMART") {
+    Nikkei = inventory[i].type;
+  }
+}
+console.log(Nikkei)
+let div2 = document.getElementById("type2");
+div2.innerText= Nikkei
+
+// Helaas kom ik maar niet door opdracht 5 heen....
